ec7bd42b44a083c8fbcd83b431e78b52fc6888f8
younes ait m'ha <younesaitmha@gmail.com>
2020-07-06 14:32:14 +0100

    add circular queue

--- data_structures/queue/circular_queue.py
+++ data_structures/queue/circular_queue.py
@@ -6,19 +6,28 @@ class CircularQueue:
         to make a circle. It is also called ‘Ring Buffer’. """
 
     def __init__(self, maxSize = 100):
-        self.queue = [None] * self.maxSize
-        self.front = None
-        self.rear = None
         self.maxSize = maxSize
+        self.queue = [None]*self.maxSize
+        self.front = 0
+        self.rear = -1
+        self.size = 0
 
     def __str__(self):
-        return [self.queue[item] for item in range(self.rear%self.maxSize, self.front%self.maxSize)]
+        if self.size == 0:
+            return str("queue is empty!")
+        else:
+            l = list()
+            index = self.rear
+            for i in range(self.size):
+                l.append(self.queue[index])
+                index = (index + 1)%self.maxSize
+            return str(l)
 
     def __eq__(self, value):
-        return self.queue == value.queue and self.maxsize == value.maxsize
+        return self.queue == value.queue and self.maxSize == value.maxSize
 
     def __len__(self):
-        return len(self.queue)
+        return len(self.size)
 
     def __contains__(self, item) -> bool:
         """ Check if an item is in the queue. """
@@ -28,45 +37,71 @@ class CircularQueue:
         return ['__init__', '__str__', '__eq__', '__len__', '__contains__', '__dir__',
                 'enqueue', 'dequeue', 'isempty', 'isfull', 'clear', 'getFront', 'getRear']
 
-    def enqueue(self):
+    def enqueue(self, element):
         """ add an element in the rear of the queue """
         if self.isfull():
-            return False
+            raise Exception('queue is full!')
         else:
-            pass
+            self.rear = (self.rear + 1)%self.maxSize
+            self.queue[self.rear] = element
+            self.size += 1
+            return True
 
     def dequeue(self):
         """ retrieves and removes the front of the queue, or returns null if this queue is empty. """
         if self.isempty():
-            return None
+            raise Exception('queue is empty!')
         else:
             d = self.queue[self.front]
-            self.queue[self.front%self.maxSize] = None
             self.front = (self.front + 1) % self.maxSize
+            self.size -= 1
             return d
 
     def getFront(self):
         """ Gets the element at the front of the queue without removing it. """
-        if self.front is None:
+        if self.size == 0:
             return None
-        else:
-            return self.queue[self.front%self.maxSize]
+        return self.queue[self.front%self.maxSize]
 
     def getRear(self):
         """ Gets the element at the rear of the queue without removing it. """
-        if self.rear is None:
+        if self.size == 0:
             return None
-        else:
-            return self.queue[self.rear%self.maxSize]
+        return self.queue[self.rear%self.maxSize]
 
     def isfull(self):
         """ Checks if the queue is full."""
+        if self.size == self.maxSize:
+            return True
+        return False
+
 
     def isempty(self):
         """ Checks if the queue is empty. """
+        if(self.size == 0):
+            return True
+        return False
 
     def clear(self):
         """ Removes all elements from the queue."""
         self.queue = [None] * self.maxSize
         self.front = None
         self.rear = None
+        self.size = 0
+
+
+if __name__ == "__main__":
+    q = CircularQueue(5)
+    q1 = CircularQueue(5)
+    q.enqueue(1)
+    q.enqueue(0)
+    q.enqueue(8)
+    q.enqueue(1)
+    q.enqueue(5)
+    print(q)
+    print(q.getRear())
+    print(q.getFront())
+    q.dequeue()
+    print(q)
+    print(q.getRear())
+    print(q.getFront())
